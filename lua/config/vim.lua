502bb59eb0b58c58032b7554a9c72ffd3f66a202
GCdM <59828466+GCdM@users.noreply.github.com>
2025-09-03 23:25:37 +0100

    Merge vim.lua from kickstarter w/ maidiyi

--- lua/config/vim.lua
+++ lua/config/vim.lua
@@ -1,76 +1,41 @@
--- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
-
 -- [[ Setting options ]]
--- See `:help vim.o`
--- NOTE: You can change these options as you wish!
---  For more options, you can see `:help option-list`
+-- NOTE: See `:help vim.opt`
+-- For more options, you can see `:help option-list`
 
--- Make line numbers default
-vim.o.number = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
--- vim.o.relativenumber = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
 
--- Enable mouse mode, can be useful for resizing splits for example!
-vim.o.mouse = 'a'
+vim.opt.scrolloff = 8
+vim.opt.showmode = false
+vim.opt.wrap = false
 
--- Don't show the mode, since it's already in the status line
-vim.o.showmode = false
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
 
--- Sync clipboard between OS and Neovim.
---  Schedule the setting after `UiEnter` because it can increase startup-time.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
-vim.schedule(function()
-  vim.o.clipboard = 'unnamedplus'
-end)
+-- vim.opt.hidden = false
+vim.opt.swapfile = false
+-- vim.opt.updatetime = 250
+vim.opt.undofile = true
+vim.opt.undodir = os.getenv('HOME') .. '/.vim/backup'
+vim.opt.backupdir = os.getenv('HOME') .. '/.vim/undo'
 
--- Enable break indent
-vim.o.breakindent = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.inccommand = 'split'
 
--- Save undo history
-vim.o.undofile = true
+vim.opt.list = true
+vim.opt.listchars = { tab = '  ', trail = '·', nbsp = '␣' }
+vim.opt.signcolumn = 'yes'
+vim.opt.breakindent = true
 
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-vim.o.ignorecase = true
-vim.o.smartcase = true
+vim.opt.timeoutlen = 500
 
--- Keep signcolumn on by default
-vim.o.signcolumn = 'yes'
+vim.opt.splitright = true
+vim.opt.splitbelow = true
 
--- Decrease update time
-vim.o.updatetime = 250
+vim.g.have_nerd_font = true
 
--- Decrease mapped sequence wait time
-vim.o.timeoutlen = 300
-
--- Configure how new splits should be opened
-vim.o.splitright = true
-vim.o.splitbelow = true
-
--- Sets how neovim will display certain whitespace characters in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
---
---  Notice listchars is set using `vim.opt` instead of `vim.o`.
---  It is very similar to `vim.o` but offers an interface for conveniently interacting with tables.
---   See `:help lua-options`
---   and `:help lua-options-guide`
-vim.o.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-
--- Preview substitutions live, as you type!
-vim.o.inccommand = 'split'
-
--- Show which line your cursor is on
-vim.o.cursorline = true
-
--- Minimal number of screen lines to keep above and below the cursor.
-vim.o.scrolloff = 10
-
--- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
--- instead raise a dialog asking if you wish to save the current file(s)
--- See `:help 'confirm'`
-vim.o.confirm = true
-
+vim.opt.confirm = true
