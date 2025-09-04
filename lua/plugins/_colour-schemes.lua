f7e1b32d0334b4f658711c65bb0dcd3f31fafc8d
GCdM <59828466+GCdM@users.noreply.github.com>
2025-09-04 21:16:29 +0100

    Add colour schemes

---
+++ lua/plugins/_colour-schemes.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function() 
+      vim.cmd.colorscheme("kanagawa")
+    end,
+  },
+  { "folke/tokyonight.nvim" },
+  { "thesimonho/kanagawa-paper.nvim" }
+}
+
