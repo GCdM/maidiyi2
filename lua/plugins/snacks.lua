fdaae412a9191650781d5b7d5eedb40b6a56aff9
GCdM <59828466+GCdM@users.noreply.github.com>
2025-09-04 22:40:07 +0100

    Add [S]earch keymaps from snacks

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -44,9 +44,9 @@ return {
   keys = {
     -- -- Top Pickers & Explorer
     { "\\", function() Snacks.explorer() end, desc = "File Explorer" },
+    { "<leader>/", function() Snacks.picker.grep() end, desc = "Project Grep" },
     { "<leader>sf", function() Snacks.picker.smart() end, desc = "[S]earch [F]iles" },
     { "<leader>sb", function() Snacks.picker.buffers() end, desc = "[S]earch [B]uffers" },
-    { "<leader>/", function() Snacks.picker.grep() end, desc = "Project Grep" },
     { "<leader>nh", function() Snacks.picker.notifications() end, desc = "[N]otification [H]istory" },
     -- { "<leader>:", function() Snacks.picker.command_history() end, desc = "Command History" },
 
@@ -74,27 +74,28 @@ return {
     -- { "<leader>sw", function() Snacks.picker.grep_word() end, desc = "Visual selection or word", mode = { "n", "x" } },
 
     -- -- search
+    { "<leader>sb", function() Snacks.picker.lines() end, desc = "[S]earch [B]uffer" },
+    { "<leader>sc", function() Snacks.picker.commands() end, desc = "[S]earch [C]ommands" },
+    { "<leader>sd", function() Snacks.picker.diagnostics() end, desc = "[S]earch [D]iagnostics" },
+    { "<leader>sD", function() Snacks.picker.diagnostics_buffer() end, desc = "[S]earch Buffer [*D*]iagnostics" },
+    { "<leader>sh", function() Snacks.picker.help() end, desc = "[S]earch [H]elp" },
+    { "<leader>si", function() Snacks.picker.icons() end, desc = "[S]earch [I]cons" },
+    { "<leader>sj", function() Snacks.picker.jumps() end, desc = "[S]earch [J]umps" },
+    { "<leader>sk", function() Snacks.picker.keymaps() end, desc = "[S]earch [K]eymaps" },
+    { "<leader>sp", function() Snacks.picker.lazy() end, desc = "[S]earch [P]lugins" },
+    { "<leader>sq", function() Snacks.picker.qflist() end, desc = "[S]earch [Q]uickfix" },
+    { "<leader>sR", function() Snacks.picker.resume() end, desc = "[S]earch [R]esume" },
+
     -- { '<leader>s"', function() Snacks.picker.registers() end, desc = "Registers" },
     -- { '<leader>s/', function() Snacks.picker.search_history() end, desc = "Search History" },
     -- { "<leader>sa", function() Snacks.picker.autocmds() end, desc = "Autocmds" },
-    -- { "<leader>sb", function() Snacks.picker.lines() end, desc = "Buffer Lines" },
-    -- { "<leader>sc", function() Snacks.picker.command_history() end, desc = "Command History" },
-    -- { "<leader>sC", function() Snacks.picker.commands() end, desc = "Commands" },
-    -- { "<leader>sd", function() Snacks.picker.diagnostics() end, desc = "Diagnostics" },
-    -- { "<leader>sD", function() Snacks.picker.diagnostics_buffer() end, desc = "Buffer Diagnostics" },
-    -- { "<leader>sh", function() Snacks.picker.help() end, desc = "Help Pages" },
+    -- { "<leader>sC", function() Snacks.picker.command_history() end, desc = "Command History" },
     -- { "<leader>sH", function() Snacks.picker.highlights() end, desc = "Highlights" },
-    -- { "<leader>si", function() Snacks.picker.icons() end, desc = "Icons" },
-    -- { "<leader>sj", function() Snacks.picker.jumps() end, desc = "Jumps" },
-    -- { "<leader>sk", function() Snacks.picker.keymaps() end, desc = "Keymaps" },
     -- { "<leader>sl", function() Snacks.picker.loclist() end, desc = "Location List" },
     -- { "<leader>sm", function() Snacks.picker.marks() end, desc = "Marks" },
     -- { "<leader>sM", function() Snacks.picker.man() end, desc = "Man Pages" },
-    -- { "<leader>sp", function() Snacks.picker.lazy() end, desc = "Search for Plugin Spec" },
-    -- { "<leader>sq", function() Snacks.picker.qflist() end, desc = "Quickfix List" },
-    -- { "<leader>sR", function() Snacks.picker.resume() end, desc = "Resume" },
-    -- { "<leader>su", function() Snacks.picker.undo() end, desc = "Undo History" },
-    -- { "<leader>uC", function() Snacks.picker.colorschemes() end, desc = "Colorschemes" },
+    { "<leader>su", function() Snacks.picker.undo() end, desc = "Undo History" },
+    { "<leader>uC", function() Snacks.picker.colorschemes() end, desc = "Colorschemes" },
 
     -- -- LSP
     -- { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" },
