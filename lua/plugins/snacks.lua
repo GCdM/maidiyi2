213518cf526e48994bc380e79b51086ca410b98b
GCdM <59828466+GCdM@users.noreply.github.com>
2025-09-04 19:59:59 +0100

    Add default snacks modules

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -6,13 +6,16 @@ return {
   opts = {
     -- bigfile = { enabled = true },
     -- dashboard = { enabled = true },
-    explorer = {},
-    -- indent = { enabled = true },
-    -- input = { enabled = true },
-    -- notifier = {
-    --   enabled = true,
-    --   timeout = 3000,
-    -- },
+
+    explorer = { enabled = true },
+    indent = { enabled = true },
+    input = { enabled = true },
+
+    notifier = {
+      enabled = true,
+      timeout = 3000,
+    },
+
     picker = {
       sources = {
         explorer = {
@@ -25,16 +28,18 @@ return {
         },
       },
     },
+
     -- quickfile = { enabled = true },
-    -- scope = { enabled = true },
-    -- scroll = { enabled = true },
-    -- statuscolumn = { enabled = true },
+    scope = { enabled = true },
+    scroll = { enabled = true },
+    statuscolumn = { enabled = true },
     -- words = { enabled = true },
-    -- styles = {
-    --   notification = {
-    --     -- wo = { wrap = true } -- Wrap notifications
-    --   }
-    -- }
+
+    styles = {
+      notification = {
+        wo = { wrap = true } -- Wrap notifications
+      }
+    }
   },
   keys = {
     -- -- Top Pickers & Explorer
@@ -44,6 +49,7 @@ return {
     -- { "<leader>:", function() Snacks.picker.command_history() end, desc = "Command History" },
     -- { "<leader>n", function() Snacks.picker.notifications() end, desc = "Notification History" },
     { "\\", function() Snacks.explorer() end, desc = "File Explorer" },
+
     -- -- find
     -- { "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers" },
     -- { "<leader>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Config File" },
@@ -51,6 +57,7 @@ return {
     -- { "<leader>fg", function() Snacks.picker.git_files() end, desc = "Find Git Files" },
     -- { "<leader>fp", function() Snacks.picker.projects() end, desc = "Projects" },
     -- { "<leader>fr", function() Snacks.picker.recent() end, desc = "Recent" },
+
     -- -- git
     -- { "<leader>gb", function() Snacks.picker.git_branches() end, desc = "Git Branches" },
     -- { "<leader>gl", function() Snacks.picker.git_log() end, desc = "Git Log" },
@@ -59,11 +66,13 @@ return {
     -- { "<leader>gS", function() Snacks.picker.git_stash() end, desc = "Git Stash" },
     -- { "<leader>gd", function() Snacks.picker.git_diff() end, desc = "Git Diff (Hunks)" },
     -- { "<leader>gf", function() Snacks.picker.git_log_file() end, desc = "Git Log File" },
+
     -- -- Grep
     -- { "<leader>sb", function() Snacks.picker.lines() end, desc = "Buffer Lines" },
     -- { "<leader>sB", function() Snacks.picker.grep_buffers() end, desc = "Grep Open Buffers" },
     -- { "<leader>sg", function() Snacks.picker.grep() end, desc = "Grep" },
     -- { "<leader>sw", function() Snacks.picker.grep_word() end, desc = "Visual selection or word", mode = { "n", "x" } },
+
     -- -- search
     -- { '<leader>s"', function() Snacks.picker.registers() end, desc = "Registers" },
     -- { '<leader>s/', function() Snacks.picker.search_history() end, desc = "Search History" },
@@ -86,6 +95,7 @@ return {
     -- { "<leader>sR", function() Snacks.picker.resume() end, desc = "Resume" },
     -- { "<leader>su", function() Snacks.picker.undo() end, desc = "Undo History" },
     -- { "<leader>uC", function() Snacks.picker.colorschemes() end, desc = "Colorschemes" },
+
     -- -- LSP
     -- { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" },
     -- { "gD", function() Snacks.picker.lsp_declarations() end, desc = "Goto Declaration" },
@@ -94,6 +104,7 @@ return {
     -- { "gy", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
     -- { "<leader>ss", function() Snacks.picker.lsp_symbols() end, desc = "LSP Symbols" },
     -- { "<leader>sS", function() Snacks.picker.lsp_workspace_symbols() end, desc = "LSP Workspace Symbols" },
+
     -- -- Other
     -- { "<leader>z",  function() Snacks.zen() end, desc = "Toggle Zen Mode" },
     -- { "<leader>Z",  function() Snacks.zen.zoom() end, desc = "Toggle Zoom" },
@@ -109,51 +120,33 @@ return {
     -- { "<c-_>",      function() Snacks.terminal() end, desc = "which_key_ignore" },
     -- { "]]",         function() Snacks.words.jump(vim.v.count1) end, desc = "Next Reference", mode = { "n", "t" } },
     -- { "[[",         function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference", mode = { "n", "t" } },
-    -- {
-    --   "<leader>N",
-    --   desc = "Neovim News",
-    --   function()
-    --     Snacks.win({
-    --       file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
-    --       width = 0.6,
-    --       height = 0.6,
-    --       wo = {
-    --         spell = false,
-    --         wrap = false,
-    --         signcolumn = "yes",
-    --         statuscolumn = " ",
-    --         conceallevel = 3,
-    --       },
-    --     })
-    --   end,
-    -- }
   },
-  init = function()
-    -- vim.api.nvim_create_autocmd("User", {
-    --   pattern = "VeryLazy",
-    --   callback = function()
-    --     -- Setup some globals for debugging (lazy-loaded)
-    --     _G.dd = function(...)
-    --       Snacks.debug.inspect(...)
-    --     end
-    --     _G.bt = function()
-    --       Snacks.debug.backtrace()
-    --     end
-    --     vim.print = _G.dd -- Override print to use snacks for `:=` command
-    --
-    --     -- Create some toggle mappings
-    --     Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
-    --     Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
-    --     Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
-    --     Snacks.toggle.diagnostics():map("<leader>ud")
-    --     Snacks.toggle.line_number():map("<leader>ul")
-    --     Snacks.toggle.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map("<leader>uc")
-    --     Snacks.toggle.treesitter():map("<leader>uT")
-    --     Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<leader>ub")
-    --     Snacks.toggle.inlay_hints():map("<leader>uh")
-    --     Snacks.toggle.indent():map("<leader>ug")
-    --     Snacks.toggle.dim():map("<leader>uD")
-    --   end,
-    -- })
-  end,
+  -- init = function()
+  --   vim.api.nvim_create_autocmd("User", {
+  --     pattern = "VeryLazy",
+  --     callback = function()
+  --       -- Setup some globals for debugging (lazy-loaded)
+  --       _G.dd = function(...)
+  --         Snacks.debug.inspect(...)
+  --       end
+  --       _G.bt = function()
+  --         Snacks.debug.backtrace()
+  --       end
+  --       vim.print = _G.dd -- Override print to use snacks for `:=` command
+  --
+  --       -- Create some toggle mappings
+  --       Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
+  --       Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
+  --       Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
+  --       Snacks.toggle.diagnostics():map("<leader>ud")
+  --       Snacks.toggle.line_number():map("<leader>ul")
+  --       Snacks.toggle.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map("<leader>uc")
+  --       Snacks.toggle.treesitter():map("<leader>uT")
+  --       Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<leader>ub")
+  --       Snacks.toggle.inlay_hints():map("<leader>uh")
+  --       Snacks.toggle.indent():map("<leader>ug")
+  --       Snacks.toggle.dim():map("<leader>uD")
+  --     end,
+  --   })
+  -- end,
 }
