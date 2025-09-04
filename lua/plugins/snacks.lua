cefc8ea6ee544177da6ec6cccb6ffb00bcd4ba09
GCdM <59828466+GCdM@users.noreply.github.com>
2025-09-04 22:44:17 +0100

    Add [G]oto keymaps from snacks

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -84,7 +84,7 @@ return {
     { "<leader>sk", function() Snacks.picker.keymaps() end, desc = "[S]earch [K]eymaps" },
     { "<leader>sp", function() Snacks.picker.lazy() end, desc = "[S]earch [P]lugins" },
     { "<leader>sq", function() Snacks.picker.qflist() end, desc = "[S]earch [Q]uickfix" },
-    { "<leader>sR", function() Snacks.picker.resume() end, desc = "[S]earch [R]esume" },
+    { "<leader>sr", function() Snacks.picker.resume() end, desc = "[S]earch [R]esume" },
 
     -- { '<leader>s"', function() Snacks.picker.registers() end, desc = "Registers" },
     -- { '<leader>s/', function() Snacks.picker.search_history() end, desc = "Search History" },
@@ -94,15 +94,15 @@ return {
     -- { "<leader>sl", function() Snacks.picker.loclist() end, desc = "Location List" },
     -- { "<leader>sm", function() Snacks.picker.marks() end, desc = "Marks" },
     -- { "<leader>sM", function() Snacks.picker.man() end, desc = "Man Pages" },
-    { "<leader>su", function() Snacks.picker.undo() end, desc = "Undo History" },
+    -- { "<leader>su", function() Snacks.picker.undo() end, desc = "Undo History" },
     { "<leader>uC", function() Snacks.picker.colorschemes() end, desc = "Colorschemes" },
 
     -- -- LSP
-    -- { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" },
-    -- { "gD", function() Snacks.picker.lsp_declarations() end, desc = "Goto Declaration" },
-    -- { "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
-    -- { "gI", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
-    -- { "gy", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
+    { "gd", function() Snacks.picker.lsp_definitions() end, desc = "[G]oto Definition" },
+    { "gD", function() Snacks.picker.lsp_declarations() end, desc = "[G]oto Declaration" },
+    { "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "[G]oto [R]eferences" },
+    { "gI", function() Snacks.picker.lsp_implementations() end, desc = "[G]oto [I]mplementation" },
+    { "gt", function() Snacks.picker.lsp_type_definitions() end, desc = "[G]oto [T]ype Definition" },
     -- { "<leader>ss", function() Snacks.picker.lsp_symbols() end, desc = "LSP Symbols" },
     -- { "<leader>sS", function() Snacks.picker.lsp_workspace_symbols() end, desc = "LSP Workspace Symbols" },
 
