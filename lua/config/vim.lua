7f0fcdad2bc5ef1516b6c9a06752dd5ea8f524de
GCdM <59828466+GCdM@users.noreply.github.com>
2025-09-13 23:45:54 +0100

    Configure statusline = 3
    This makes there be only one statusline showing the currently active
    window

--- lua/config/vim.lua
+++ lua/config/vim.lua
@@ -26,6 +26,7 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.inccommand = 'split'
 
+vim.opt.laststatus = 3
 vim.opt.list = true
 vim.opt.listchars = { tab = '  ', trail = '·', nbsp = '␣' }
 vim.opt.signcolumn = 'yes'
