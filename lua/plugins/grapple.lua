6fa33c285378d7af83c386daf920531d351bb124
GCdM <59828466+GCdM@users.noreply.github.com>
2025-10-16 23:09:51 +0100

    Add grapple.nvim spec

---
+++ lua/plugins/grapple.lua
@@ -0,0 +1,17 @@
+return {
+  "cbochs/grapple.nvim",
+  dependencies = {
+    { "nvim-tree/nvim-web-devicons", lazy = true }
+  },
+  opts = {
+    scope = "git",
+  },
+  event = { "BufReadPost", "BufNewFile" },
+  cmd = "Grapple",
+  keys = {
+    { "<leader>m", "<cmd>Grapple toggle<cr>", desc = "Grapple toggle tag" },
+    { "<leader>M", "<cmd>Grapple toggle_tags<cr>", desc = "Grapple open tags window" },
+    { "<leader>]", "<cmd>Grapple cycle_tags next<cr>", desc = "Grapple cycle next tag" },
+    { "<leader>[", "<cmd>Grapple cycle_tags prev<cr>", desc = "Grapple cycle prev tag" },
+  },
+}
