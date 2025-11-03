0504b5349560b42240d50fc0c0399890f2593205
GCdM <59828466+GCdM@users.noreply.github.com>
2025-11-03 09:19:06 +0000

    Add ts_ls configuration

---
+++ lsp/ts_ls.lua
@@ -0,0 +1,44 @@
+return {
+	cmd = { "typescript-language-server", "--stdio" },
+	filetypes = {
+		"javascript",
+		"javascriptreact",
+		"javascript.jsx",
+		"typescript",
+		"typescriptreact",
+		"typescript.tsx",
+	},
+	root_dir = function(fname)
+		return vim.fs.root(fname, { "package.json", "tsconfig.json", "jsconfig.json", ".git" })
+	end,
+	single_file_support = true,
+	-- Disable diagnostics to avoid conflicts with biome
+	-- ts_ls will still provide language features (autocomplete, go-to-definition, etc.)
+	handlers = {
+		["textDocument/publishDiagnostics"] = function() end,
+	},
+	settings = {
+		typescript = {
+			inlayHints = {
+				includeInlayParameterNameHints = "all",
+				includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+				includeInlayFunctionParameterTypeHints = true,
+				includeInlayVariableTypeHints = true,
+				includeInlayPropertyDeclarationTypeHints = true,
+				includeInlayFunctionLikeReturnTypeHints = true,
+				includeInlayEnumMemberValueHints = true,
+			},
+		},
+		javascript = {
+			inlayHints = {
+				includeInlayParameterNameHints = "all",
+				includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+				includeInlayFunctionParameterTypeHints = true,
+				includeInlayVariableTypeHints = true,
+				includeInlayPropertyDeclarationTypeHints = true,
+				includeInlayFunctionLikeReturnTypeHints = true,
+				includeInlayEnumMemberValueHints = true,
+			},
+		},
+	},
+}
